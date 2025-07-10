642a3e37df950b5fe519619f74e42a3cd73c4b5b
mywealth360 <mywealth360family@gmail.com>
2025-07-09 22:10:42 -0300

    Updated package.json

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,6 @@
 import js from '@eslint/js';
 import globals from 'globals';
-import reactHooks from 'eslint-plugin-react-hooks';
+import { reactHooks } from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 import tseslint from 'typescript-eslint';
 
@@ -18,7 +18,7 @@ export default tseslint.config(
       'react-refresh': reactRefresh,
     },
     rules: {
-      ...reactHooks.configs.recommended.rules,
+      ...reactHooks.configs['recommended-latest'].rules,
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true },
